387c202683e1f3d860e1218010a714819a1b0c8a
ullash4 <programmerullash@gmail.com>
2022-04-27 15:24:56 +0600

    pushing for heroku

---
+++ heroku_deploy_steps.js
@@ -0,0 +1,12 @@
+/**
+ *  One time for my computer
+ * --------------------------
+ * 1. Create account (one time)
+ * 2. Verify email (one time)
+ * 3. Install heroku cli (one time)
+ * 4. heroku login
+ * 
+ * For each projects one time
+ * ---------------------------
+ * 1. heroku create
+ */
